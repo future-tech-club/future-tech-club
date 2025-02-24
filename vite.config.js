12e0b981fdbb9bdf5ea8adbad52a36954bcc8c05
future-tech-club <future_tech_club@inptic.edu.dz>
2025-02-24 09:51:49 +0100

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ftc-website-frontend/',  
+  base: '/future-tech-club/',  
   build: {
     outDir: 'dist',
   },
   server: {
     historyApiFallback: true,
   },
+  assetsInclude: ['**/*.woff', '**/*.ttf'],
 });
